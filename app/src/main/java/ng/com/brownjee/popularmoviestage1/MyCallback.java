90f714bc16494fe5676258c7d8737de7be9f1961
Emem Brownson <brownjee001@gmail.com>
2017-04-21 17:21:56 +0100

    Initial commit

---
+++ app/src/main/java/ng/com/brownjee/popularmoviestage1/MyCallback.java
@@ -0,0 +1,14 @@
+package ng.com.brownjee.popularmoviestage1;
+
+
+
+import ng.com.brownjee.popularmoviestage1.parser.Movies;
+
+/**
+ * Created by brownjee on 4/21/2017.
+ */
+
+public interface MyCallback {
+
+    void updateAdapter(Movies[] movies);
+}
